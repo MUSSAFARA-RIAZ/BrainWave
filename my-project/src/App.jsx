9ad5a4db167a47c9994684ea7c7c0589eedd1c73
MUSSAFARA-RIAZ <mussafarariaz@gmail.com>
2024-10-09 23:24:46 +0500

    first commit : tailwind configuration completed

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-3xl text-yellow-600 text-center'>
+
+
+      Hello
+
+
+      </h1>
+    </div>
+  )
+}
